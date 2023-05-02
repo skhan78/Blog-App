e69112cdf1545a2416f23cb1d8ccb5721991730c
shahid <shkhan@bestpeers.com>
2023-05-02 17:45:48 +0530

    Set default props and pass

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
-function Navbar(){
+import React from "react";
+import PropTypes from "prop-types";
+
+function Navbar(props){
   return(
   <div>
   <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-fluid">
-      <a className="navbar-brand" href='/'>MyApp</a>
+      <a className="navbar-brand" href='/'>{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -13,7 +16,7 @@ function Navbar(){
             <a className="nav-link active" aria-current="page" href='/'>Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href='/'>About</a>
+            <a className="nav-link" href='/'>{props.about_us}</a>
           </li>
         </ul>
         <form className="d-flex" role="search">
@@ -28,3 +31,6 @@ function Navbar(){
 }
 
 export default Navbar;
+
+Navbar.propTypes = {title: PropTypes.string, about_us: PropTypes.string}
+Navbar.defaultProps = {title: "you have to enter a title", about_us: "you have to enter a about"}
