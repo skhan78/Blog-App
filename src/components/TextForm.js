7925a598a0834cae400cd576553b6daf1431be48
shahid <shkhan@bestpeers.com>
2023-05-03 12:17:25 +0530

    Add count for character in app

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,12 +11,19 @@ export default function TextForm(props){
   const [text, setText]=useState("Enter your text here...");
 
   return (
-    <div>
+  <>
+    <div className="container">
       <div className="mb-3">
         <label htmlFor="myBox" className="form-label">{props.heading}</label>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary" onClick={handleUpClick}>convert to uppercase</button>
     </div>
+    <div className="container my-2">
+      <h1>Your text summary</h1>
+      <p>Characters:{text.length}</p>
+      <p>Words: {text.split(" ").length}</p>
+    </div>
+  </>
   )
 }
