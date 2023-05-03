9a7be3e26602c42e99b3e8abf235fb87416ae2f3
shahid <shkhan@bestpeers.com>
2023-05-03 15:11:18 +0530

    Added Support for dark mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
       <Navbar title="My Blog App" about_us="About Us"/>
       <div className="container my-3">
-        <TextForm heading="Enter the text"/>
+        {/* <TextForm heading="Enter the text"/> */}
+        <About/>
       </div>
     </>
   );
